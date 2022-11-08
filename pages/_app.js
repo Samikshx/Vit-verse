9e22dc475c04e3379f34481398138404ed9fbb63
Samikshx <samagrawal2905@gmail.com>
2022-11-09 00:03:19 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import "../styles/globals.css";
 
 //INTERNAL IMPORT
-import {NavBar} from "../components/comp_index";
+import {NavBar , Footer} from "../components/comp_index";
 
 const MyApp = ({ Component, pageProps }) => (
   <div>
       <NavBar />
       <Component {...pageProps} />
+      <Footer />
   </div>
 );
 
