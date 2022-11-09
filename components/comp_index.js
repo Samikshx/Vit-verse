2157b8e55f95cd9d1b43e25720e351b67bcb188f
Samikshx <samagrawal2905@gmail.com>
2022-11-10 01:29:46 +0530

    first commit

--- components/comp_index.js
+++ components/comp_index.js
@@ -3,4 +3,5 @@ import Footer from "./footer/footer";
 import NavBar from "./navBar/navbar";
 import HeroSection from "./HeroSection/HeroSection";
 import Services from "./Service/Service";
-export {Button, Footer, NavBar, HeroSection, Services};
+import BigNFTSilder from "./BigNFTSilder/BigNFTSilder";
+export {Button, Footer, NavBar, HeroSection, Services, BigNFTSilder};
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 //Internal import
 import Style from "../styles/index.module.css";
-import { HeroSection, Services } from '../components/comp_index';
+import { HeroSection, Services,BigNFTSilder } from '../components/comp_index';
 const Home = () => {
   return(
     <div className= {Style.homePage}>
       <HeroSection/>
       <Services/>
+      <BigNFTSilder/>
     </div>
   );
 };
